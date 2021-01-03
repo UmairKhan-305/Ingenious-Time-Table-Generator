4ae3e95603fe077ef9deea624e9fdfefabae64ff
UmairKhan-305 <bscs2012305@szabist.pk>
2021-01-03 18:39:48 +0500

    Presentable Printing

--- Koshish.c
+++ Koshish.c
@@ -23,12 +23,23 @@ int main()
         printf("Couldn't open %s for reading\n");
         return 0;
     }
+
+    printf("Semester Details:\n");
+
     while(fgets(line, sizeof(line), infile) != NULL) {
-        sscanf(line, "%[^\t]\t%[^\t]\t%[^\t]\t%[^\t]\t%[^\t]\t%[^\t]\t%[^\t]\t%[^\n]", 
+        sscanf(line, "%[^\t]\t%[^\t]\t%[^\t]\t%[^\t]\t%[^\t]\t%[^\t]\t%[^\t]\t%[^\t]\t%[^\n]", 
         timeTable.courseCode[i], timeTable.subjectName[i], timeTable.teachers[i][0], timeTable.teachers[i][1], timeTable.teachers[i][2], timeTable.teachers[i][3], timeTable.teachers[i][4], timeTable.teachers[i][5], timeTable.teachers[i][6]);
-        printf("%s\n", timeTable.courseCode[i]);
-        printf("%s\n", timeTable.subjectName[i]);
-        printf("%s\n", timeTable.teachers[i][0]);
+        
+        printf("%s\t | ", timeTable.courseCode[i]);
+        printf("%s\t | ", timeTable.subjectName[i]);
+        
+        for(int j = 0; j < 7; j++){
+            printf("%s\t | ", timeTable.teachers[i][j]);
+        }
+
+        printf("\n");
+        printf("__________________________________________________________\n");
+
         i++; 
     }
 }
